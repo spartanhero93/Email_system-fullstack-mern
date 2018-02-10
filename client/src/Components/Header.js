e32aef2a928e9f54153c6fcb8b2013d4a789a48e
Luis Lopez <spartanhero93@gmail.com>
2018-02-10 01:08:13 -0500

    stage managment with redux, thunk middleware, and added react router

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
-export default class Header extends Component{
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li><a href='/auth/google'>Login with Google</a></li>
+        );
+
+      default:
+        return <li><a href='/api/logout'>Logout</a></li>;
+    }
+  }
+
+
   render(){
     return (
       <nav>
         <div className="nav-wrapper">
           <div className="row">
             <div className="col s12">
-              <a href="#" className="left brand-logo">
+              <Link 
+                to={this.props.auth ? '/surveys': '/'} 
+                className="left brand-logo"
+              >
                 Emaily
-              </a>
+              </Link>
               <ul className="right">
-                <li>
-                  <a>Login with Google</a>
-                </li>
+                { this.renderContent() }
               </ul>
             </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(Header)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,22 @@
 const passport = require("passport");
 
 module.exports = app => {
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-      scope: ["profile", "email"]
-    })
-  );
+  app.get("/auth/google", passport.authenticate("google", {
+      scope: ["profile", "email"],
+      prompt: "select_account"
+    }));
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback", 
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+);
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get("/api/current_user", (req, res) => {
