30fa2662542c3b786c875c455dd157159d53236b
Luis Lopez <spartanhero93@gmail.com>
2018-02-13 20:07:08 -0500

    Added billing and client sid app

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if(process.env.NODE_ENV === "production"){
+  //Express serving up production assetss
+  app.use(express.static("client/build"));
+
+  //Express serving HTML file 
+  //incase it doesnt recoginze the route
+  const path = require("path");
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
